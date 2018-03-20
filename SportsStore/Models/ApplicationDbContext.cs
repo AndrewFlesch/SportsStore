6e4884c3228922d3fcad59bead0c16eb94214d14
AndrewFlesch <aflesch@outlook.com>
2018-03-20 15:28:50 -0500

    Added Database

--- SportsStore/Models/ApplicationDbContext.cs
+++ SportsStore/Models/ApplicationDbContext.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace SportsStore.Models
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ SportsStore/Models/SeedData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices.GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+            if(!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Category = "Watersports",
+                        Price = 275
+                    },
+                     new Product
+                     {
+                         Name = "Lifejacket",
+                         Description = "A boat for one person",
+                         Category = "Watersports",
+                         Price = 48.95m
+                     },
+                      new Product
+                      {
+                          Name = "Soccer Ball",
+                          Description = "A boat for one person",
+                          Category = "Watersports",
+                          Price = 19.50m
+                      }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
