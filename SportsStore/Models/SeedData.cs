4338ea7bb81429e4584b1b9dd81ce1e0b5a5924a
AndrewFlesch <aflesch@outlook.com>
2018-03-21 14:20:35 -0500

    Paging and TagHelper

--- SportsStore/Models/SeedData.cs
+++ SportsStore/Models/SeedData.cs
@@ -31,11 +31,25 @@ namespace SportsStore.Models
                          Category = "Watersports",
                          Price = 48.95m
                      },
+                     new Product
+                     {
+                         Name = "Bench Press",
+                         Description = "used for weight lifting",
+                         Category = "Fitness",
+                         Price = 20.95m
+                     },
+                      new Product
+                      {
+                          Name = "Punching Bag",
+                          Description = "used for training",
+                          Category = "Boxing",
+                          Price = 20.95m
+                      },
                       new Product
                       {
                           Name = "Soccer Ball",
                           Description = "A boat for one person",
-                          Category = "Watersports",
+                          Category = "Sports",
                           Price = 19.50m
                       }
                     );
