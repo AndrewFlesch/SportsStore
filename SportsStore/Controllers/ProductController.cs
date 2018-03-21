0c5478b5c312902ce5e94ce41eb38dcffc165264
AndrewFlesch <aflesch@outlook.com>
2018-03-21 15:46:27 -0500

    Added Paging Models and Tag Helper

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.ViewModels;
 using SportsStore.Models;
 
 namespace SportsStore.Controllers
@@ -11,13 +12,25 @@ namespace SportsStore.Controllers
     {
         private IProductRepository repository;
         public int PageSize = 4;
+
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List(int productPage=1) => View(repository.Products.OrderBy(p => p.ProductID)
+        public ViewResult List(int productPage=1) => View(new ProductsListViewModel
+        {
+            Products = repository.Products
+            .OrderBy(p => p.ProductID)
             .Skip((productPage -1)*PageSize)
-            .Take(PageSize));
+            .Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = repository.Products.Count()
+            }
+        }
+        );
     }
 }
